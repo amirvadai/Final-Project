b0cb121ecb327e7b42421e9eae896e4f25abc598
Amir Vadai <vadai.amir@gmail.com>
2026-08-23 15:45:24 +0300

    added basic main page

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-  res.send('Server is running!');
+    res.render('feed/index');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
