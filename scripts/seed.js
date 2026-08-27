342e24c2fbf9a67480209b7b3621d5c0406959f5
Amir Vadai <vadai.amir@gmail.com>
2026-08-27 15:17:55 +0300

    Added more pepole, posts, groups to the seed

--- scripts/seed.js
+++ scripts/seed.js
@@ -52,7 +52,97 @@ async function seed() {
             avatarUrl: "/images/demo/noam-avatar.jpg"
         });
 
-        console.log("Users created");
+        const yael = await userModel.createUser({
+        username: "yael",
+        passwordHash: passwordHash,
+        displayName: "Yael Israeli",
+        city: "Netanya",
+        interests: ["Fitness", "Travel", "Food"],
+        avatarUrl: "/images/demo/yael-avatar.jpg"
+    });
+
+    const tom = await userModel.createUser({
+        username: "tom",
+        passwordHash: passwordHash,
+        displayName: "Tom Bar",
+        city: "Tel Aviv",
+        interests: ["Gaming", "Music", "Technology"],
+        avatarUrl: "/images/demo/tom-avatar.png"
+    });
+
+    const shira = await userModel.createUser({
+        username: "shira",
+        passwordHash: passwordHash,
+        displayName: "Shira Cohen",
+        city: "Jerusalem",
+        interests: ["Art", "Photography", "Fashion"],
+        avatarUrl: "/images/demo/shira-avatar.jpg"
+    });
+
+    const adam = await userModel.createUser({
+        username: "adam",
+        passwordHash: passwordHash,
+        displayName: "Adam Levi",
+        city: "Haifa",
+        interests: ["Sports", "Technology", "Gaming"],
+        avatarUrl: "/images/demo/adam-avatar.jpg"
+    });
+
+    const noa = await userModel.createUser({
+        username: "noa",
+        passwordHash: passwordHash,
+        displayName: "Noa Mizrahi",
+        city: "Netanya",
+        interests: ["Music", "Travel", "Photography"],
+        avatarUrl: "/images/demo/noa-avatar.jpg"
+    });
+
+    const idan = await userModel.createUser({
+        username: "idan",
+        passwordHash: passwordHash,
+        displayName: "Idan Shalom",
+        city: "Beer Sheva",
+        interests: ["Programming", "Gaming", "Technology"],
+        avatarUrl: "/images/demo/idan-avatar.jpg"
+    });
+
+    const lior = await userModel.createUser({
+        username: "lior",
+        passwordHash: passwordHash,
+        displayName: "Lior Ben David",
+        city: "Rishon LeZion",
+        interests: ["Running", "Fitness", "Sports"],
+        avatarUrl: "/images/demo/lior-avatar.jpg"
+    });
+
+    const dana = await userModel.createUser({
+        username: "dana",
+        passwordHash: passwordHash,
+        displayName: "Dana Katz",
+        city: "Tel Aviv",
+        interests: ["Food", "Travel", "Art"],
+        avatarUrl: "/images/demo/dana-avatar.jpg"
+    });
+
+    const omer = await userModel.createUser({
+        username: "omer",
+        passwordHash: passwordHash,
+        displayName: "Omer Azulay",
+        city: "Herzliya",
+        interests: ["Surfing", "Sports", "Travel"],
+    avatarUrl: "/images/demo/omer-avatar.jpg"
+    });
+
+    const roni = await userModel.createUser({
+        username: "roni",
+        passwordHash: passwordHash,
+        displayName: "Roni Driks",
+        city: "Beit Nehemia",
+        interests: ["Books", "Music", "Photography"],
+        avatarUrl: "/images/demo/roni-avatar.png"
+    });
+
+            console.log("Users created");
 
         //friendships
 
@@ -73,6 +163,83 @@ async function seed() {
                 }
             }
         );
+
+        await db.collection("users").updateOne(
+        { _id: daniel._id },
+        {
+            $addToSet: {
+                friends: {
+                    $each: [maya._id, lior._id, omer._id, tom._id]
+                }
+            }
+        }
+    );
+
+    await db.collection("users").updateOne(
+        { _id: maya._id },
+        {
+            $addToSet: {
+                friends: {
+                    $each: [daniel._id, shira._id, noa._id, dana._id]
+                }
+            }
+        }
+    );
+
+    await db.collection("users").updateOne(
+        { _id: noam._id },
+        {
+            $addToSet: {
+                friends: {
+                    $each: [tom._id, adam._id, idan._id]
+                }
+            }
+        }
+    );
+
+    await db.collection("users").updateOne(
+        { _id: tom._id },
+        {
+            $addToSet: {
+                friends: {
+                    $each: [noam._id, adam._id, idan._id, daniel._id]
+                }
+            }
+        }
+    );
+
+    await db.collection("users").updateOne(
+        { _id: shira._id },
+        {
+            $addToSet: {
+                friends: {
+                    $each: [maya._id, noa._id, roni._id]
+                }
+            }
+        }
+    );
+
+    await db.collection("users").updateOne(
+        { _id: lior._id },
+        {
+            $addToSet: {
+                friends: {
+                    $each: [daniel._id, yael._id, omer._id]
+                }
+            }
+        }
+    );
+
+    await db.collection("users").updateOne(
+        { _id: yael._id },
+        {
+            $addToSet: {
+                friends: {
+                    $each: [lior._id, dana._id, noa._id]
+                }
+            }
+        }
+    );
 
         //groups
 
@@ -112,6 +279,76 @@ async function seed() {
             }
         });
 
+        const gamingGroup = await groupModel.createGroup({
+        name: "Israel Gaming Hub",
+        description:
+            "A community for gamers to meet, play and talk about their favorite games.",
+        category: "Gaming",
+        manager: tom._id,
+        location: {
+            address: "Dizengoff Center",
+            city: "Tel Aviv",
+            latitude: 32.0756,
+            longitude: 34.7743
+        }
+    });
+
+    const techGroup = await groupModel.createGroup({
+        name: "Tech & Programming",
+        description:
+            "For people interested in programming, technology and building projects.",
+        category: "Technology",
+        manager: idan._id,
+        location: {
+            address: "HaShalom Road",
+            city: "Tel Aviv",
+            latitude: 32.0747,
+            longitude: 34.7933
+        }
+    });
+
+    const fitnessGroup = await groupModel.createGroup({
+        name: "Fitness Together",
+        description:
+            "Train together, share fitness tips and stay motivated.",
+        category: "Sports",
+        manager: yael._id,
+        location: {
+            address: "Independence Park",
+            city: "Netanya",
+            latitude: 32.3215,
+            longitude: 34.8532
+        }
+    });
+
+    const travelGroup = await groupModel.createGroup({
+        name: "Israel Travel Community",
+        description:
+            "Discover new places, plan trips and meet people who love traveling.",
+        category: "Travel",
+        manager: dana._id,
+        location: {
+            address: "Rothschild Boulevard",
+            city: "Tel Aviv",
+            latitude: 32.0645,
+            longitude: 34.7745
+        }
+    });
+
+    const musicGroup = await groupModel.createGroup({
+        name: "Music Lovers Israel",
+        description:
+            "Share music, discover new artists and organize music events.",
+        category: "Music",
+        manager: roni._id,
+        location: {
+            address: "Habima Square",
+            city: "Tel Aviv",
+            latitude: 32.0730,
+            longitude: 34.7816
+        }
+    });
+
         console.log("Groups created");
 
         //add members
@@ -133,6 +370,61 @@ async function seed() {
                 }
             }
         );
+
+        await db.collection("groups").updateOne(
+        { _id: gamingGroup._id },
+        {
+            $addToSet: {
+                members: {
+                    $each: [noam._id, adam._id, idan._id]
+                }
+            }
+        }
+    );
+
+    await db.collection("groups").updateOne(
+        { _id: techGroup._id },
+        {
+            $addToSet: {
+                members: {
+                    $each: [noam._id, tom._id, adam._id]
+                }
+            }
+        }
+    );
+
+    await db.collection("groups").updateOne(
+        { _id: fitnessGroup._id },
+        {
+            $addToSet: {
+                members: {
+                    $each: [daniel._id, lior._id, omer._id]
+                }
+            }
+        }
+    );
+
+    await db.collection("groups").updateOne(
+        { _id: travelGroup._id },
+        {
+            $addToSet: {
+                members: {
+                    $each: [maya._id, yael._id, noa._id, omer._id]
+                }
+            }
+        }
+    );
+
+    await db.collection("groups").updateOne(
+        { _id: musicGroup._id },
+        {
+            $addToSet: {
+                members: {
+                    $each: [daniel._id, noa._id, tom._id, roni._id]
+                }
+            }
+        }
+    );
 
         //posts
 
@@ -161,6 +453,124 @@ async function seed() {
             mediaUrl: "/videos/demo/noam-video.mp4",
             tags: ["technology"]
         });
+
+        await postModel.createPost({
+        author: tom._id,
+        group: gamingGroup._id,
+        type: "text",
+        text: "Anyone playing tonight? Looking for a squad!",
+        tags: ["gaming", "multiplayer"]
+    });
+
+    await postModel.createPost({
+        author: noam._id,
+        group: gamingGroup._id,
+        type: "image",
+        text: "My new gaming setup is finally ready!",
+        mediaUrl: "/images/demo/gaming-setup.jpg",
+        tags: ["gaming", "setup"]
+    });
+
+    await postModel.createPost({
+        author: idan._id,
+        group: techGroup._id,
+        type: "text",
+        text: "Working on a new web development project. Excited to share it soon!",
+        tags: ["programming", "webdevelopment"]
+    });
+
+    await postModel.createPost({
+        author: adam._id,
+        group: techGroup._id,
+        type: "text",
+        text: "What programming language are you learning right now?",
+        tags: ["programming", "technology"]
+    });
+
+    await postModel.createPost({
+        author: yael._id,
+        group: fitnessGroup._id,
+        type: "image",
+        text: "Great workout at the park today!",
+        mediaUrl: "/images/demo/fitness-workout.jpg",
+        tags: ["fitness", "workout"]
+    });
+
+    await postModel.createPost({
+        author: lior._id,
+        group: runnersGroup._id,
+        type: "text",
+        text: "Who is joining us for a morning run this weekend?",
+        tags: ["running", "sports"]
+    });
+
+    await postModel.createPost({
+        author: omer._id,
+        group: fitnessGroup._id,
+        type: "text",
+        text: "Don't forget to stay hydrated during your workout!",
+        tags: ["fitness", "health"]
+    });
+
+    await postModel.createPost({
+        author: dana._id,
+        group: travelGroup._id,
+        type: "image",
+        text: "One of my favorite views from a recent trip.",
+        mediaUrl: "/images/demo/travel-view.jpg",
+        tags: ["travel", "nature"]
+    });
+
+    await postModel.createPost({
+        author: maya._id,
+        group: travelGroup._id,
+        type: "text",
+        text: "What is your favorite place to visit in Israel?",
+        tags: ["travel", "israel"]
+    });
+
+    await postModel.createPost({
+        author: noa._id,
+        group: photographyGroup._id,
+        type: "image",
+        text: "Sunset photography is always worth the wait.",
+        mediaUrl: "/images/demo/sunset-photo.jpg",
+        tags: ["photography", "sunset"]
+    });
+
+    await postModel.createPost({
+        author: roni._id,
+        group: musicGroup._id,
+        type: "text",
+        text: "Recommend me a song that everyone should hear at least once.",
+        tags: ["music", "recommendations"]
+});
+
+    await postModel.createPost({
+        author: daniel._id,
+        group: null,
+        type: "text",
+        text: "Had an amazing week! Looking forward to the weekend.",
+        tags: ["life", "weekend"]
+});
+
+    await postModel.createPost({
+        author: shira._id,
+        group: null,
+        type: "image",
+        text: "Trying something new with my photography today.",
+        mediaUrl: "/images/demo/shira-photo.jpg",
+        tags: ["photography", "art"]
+});
+
+    await postModel.createPost({
+        author: adam._id,
+        group: null,
+        type: "video",
+        text: "A short clip from today's training session.",
+        mediaUrl: "/videos/demo/adam-training.mp4",
+        tags: ["sports", "training"]
+});
 
         console.log("Posts created");
 
